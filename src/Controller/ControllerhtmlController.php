8b5a62397576f4dcd4c382398c7308dc2f737fcc
amel fradj <amelfradj1@gmail.com>
2024-01-29 22:36:14 +0100

    😜 header footer ok

---
+++ src/Controller/ControllerhtmlController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ControllerhtmlController extends AbstractController
+{
+    #[Route('/controllerhtml', name: 'app_controllerhtml', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('controllerhtml/index.html.twig', [
+            'controller_name' => 'ControllerhtmlController',
+        ]);
+    }
+}
