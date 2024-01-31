52a911a24fe66daaee0c659816420a9f06a041f9
amel fradj <amelfradj1@gmail.com>
2024-01-31 15:39:05 +0100

    ok

--- src/Controller/ControllerhtmlController.php
+++ src/Controller/ControllerhtmlController.php
@@ -19,12 +19,3 @@ class ControllerhtmlController extends AbstractController
     
 }
 
-// #[Route('/registration', name: 'app_registration', methods: ['GET', 'POST'])]
-// public function Registration(): Response
-// {
-//     $utilisateur = new Utilisateur();
-//     $form = $this->createForm(RegistrationType::class, $utilisateur);
-//     return $this->render('controllerhtml/compte1.html.twig', [
-//         'form' => $form->createView(),
-//     ]);
-// }
