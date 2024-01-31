3f8e5055a3aa31917c1a3f92caa8e00285b1908c
shaimaa <shaimaa.salah79@email.com>
2024-01-31 10:59:33 +0100

    page-connexion

--- src/Controller/ControllerhtmlController.php
+++ src/Controller/ControllerhtmlController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class  extends AbstractController
+class ControllerhtmlController extends AbstractController
 {
     #[Route('/', name: 'app_controllerhtml', methods: ['GET'])]
     public function index(): Response
@@ -16,3 +17,13 @@ class  extends AbstractController
         ]);
     }
 }
+
+// #[Route('/registration', name: 'app_registration', methods: ['GET', 'POST'])]
+// public function Registration(): Response
+// {
+//     $utilisateur = new Utilisateur();
+//     $form = $this->createForm(RegistrationType::class, $utilisateur);
+//     return $this->render('controllerhtml/compte1.html.twig', [
+//         'form' => $form->createView(),
+//     ]);
+// }
